ca03656abbd87eae9160de9becd42aa9cd6f40cc
SergeyShobin <serega-shobin@mail.ru>
2021-02-12 00:02:12 +0300

    add another languages

--- lib/models/messages/messages.dart
+++ lib/models/messages/messages.dart
@@ -1,12 +1,17 @@
-import 'package:get/get.dart';
-import 'package:emma_mobile/models/messages/ru.dart';
+import 'package:emma_mobile/models/messages/de.dart';
 import 'package:emma_mobile/models/messages/en.dart';
+import 'package:emma_mobile/models/messages/es.dart';
+import 'package:emma_mobile/models/messages/fr.dart';
+import 'package:emma_mobile/models/messages/ru.dart';
+import 'package:get/get.dart';
 
 class Messages extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
-    'ru': RuMessages.messages,
-    'en': EnMessages.messages,
-
-  };
+        'ru': RuMessages.messages,
+        'en': EnMessages.messages,
+        'de': DeMessages.messages,
+        'fr': FrMessages.messages,
+        'es': EsMessages.messages,
+      };
 }
